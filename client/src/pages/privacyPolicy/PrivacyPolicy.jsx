05ee6d1764d58bb3b7223e3d9c43d18d9a06c6d4
VM031071 <valerito1973vm@gmail.com>
2025-09-27 22:10:36 -0500

    updated initial PrivacyPolicy structure

--- client/src/pages/privacyPolicy/PrivacyPolicy.jsx
+++ client/src/pages/privacyPolicy/PrivacyPolicy.jsx
@@ -115,6 +115,144 @@ function PrivacyPolicy() {
           <p>
             You are responsible for any third-party personal data obtained, published or shared through this website.
           </p>
+
+          <h2 id="ppSec4">
+            Mode and place of processing personal data
+          </h2>
+          <p>
+            <strong>Methods of processing</strong>
+            <br></br>
+            We take appropriate security measures to prevent unauthorized access, disclosure, modification, or
+            unauthorized destruction of the data. The data processing is carried out using computers and/or IT
+            enabled tools, following organizational procedures and modes strictly related to the purposes indicated. 
+          </p>
+          <p>
+            In addition to ourselves, in some cases, the data may be accessible to certain types of persons in charge,
+            involved with the operation of this website (e.g. administration, sales team, marketing team, legal team).
+          </p>
+          <p>
+            The data may also be accessible to external parties appointed, if necessary, as data processors by us.
+          </p>
+          <p>
+            The updated list of these parties may be requested from us at any time by contacting us at the contact
+            details provided in this document.
+          </p>
+          <p>
+            <strong>Place</strong>
+            <br></br>
+            The data is processed at our operating offices and in any other places where the parties involved in the
+            processing are located. Depending on your location, data transfers may involve transferring your data to
+            a country other than your own. To find out more about the place of processing of such transferred data,
+            you can check the section containing details about the processing of personal data.
+          </p>
+          <p>
+            <strong>Retention time</strong>
+            <br></br>
+            Unless specified otherwise in this document, personal data shall be processed and stored for as long as
+            required by the purpose they have been collected for and may be retained for longer due to applicable
+            legal obligation or based on your consent.
+          </p>
+
+          <h2 id="ppSec5">
+            Detailed information on the processing of personal data
+          </h2>
+          <p>
+            Your personal data is collected to allow us to provide our service, comply with our legal obligations,
+            respond to enforcement requests, protect our rights and interests (or yours or those of third parties),
+            detect any malicious or fraudulent activity, as well as the purposes set out below:
+          </p>
+          <h3>
+            Analytics
+          </h3>
+          <p>
+            The services contained in this section enable us to monitor and analyze web traffic and can be used to
+            keep track of your behavior.
+          </p>
+          <h4>
+            Google Analytics (Universal Analytics) (Google LLC)
+          </h4>
+          <p>
+            Google Analytics (Universal Analytics) is a web analysis service provided by Google LLC (“Google”).
+            Google utilizes the data collected to track and examine the use of this website, to prepare reports
+            on its activities and share them with other Google services. Google may use the data collected to
+            contextualize and personalize the ads of its own advertising network. 
+          </p>
+          <p>
+            To understand Google's use of data, consult
+            Google's <a href="https://policies.google.com/technologies/partner-sites">partner policy</a> and
+            their <a href="https://business.safety.google/privacy/">Business data page</a>.
+          </p>
+          <p>
+            <strong>Personal data processed:</strong> Trackers; Usage data
+            <br></br>
+            <strong>Place of processing:</strong> United States
+            <br></br>
+            <strong>Privacy policy:</strong>
+            <a href="https://business.safety.google/privacy/">https://business.safety.google/privacy/</a>
+            <br></br>
+            <strong>Opt-out link:</strong>
+            <a herf="https://tools.google.com/dlpage/gaoptout">https://tools.google.com/dlpage/gaoptout</a>
+          </p>
+          <p>
+            Category of personal information collected according to the CCPA: internet or other electronic
+            network activity information. This processing constitutes a sale according to the CCPA, VCDPA,
+            CPA, CTDPA and UCPA
+          </p>
+          <h3>
+            Displaying content from external platforms
+          </h3>
+          <p>
+            This type of service allows you to view content hosted on external platforms directly from the
+            pages of this website and interact with them. Such services are often referred to as widgets,
+            which are small elements placed on a website or app. They provide specific information or perform
+            a particular function and often allow for user interaction.This type of service might still
+            collect web traffic data for the pages where the service is installed, even when you do not use it.
+          </p>
+          <h4>
+            Google Fonts (Google LLC)
+          </h4>
+          <p>
+            Google Fonts is a typeface visualization service provided by Google LLC that allows this website to
+            incorporate content of this kind on its pages.
+          </p>
+          <p>
+            To understand Google's use of data, consult
+            Google's <a href="https://policies.google.com/technologies/partner-sites">partner policy</a> and
+            their <a href="https://business.safety.google/privacy/">Business data page</a>.
+          </p>
+          <p>
+            <strong>Personal data processed:</strong> Trackers; Usage data
+            <br></br>
+            <strong>Place of processing:</strong> United States
+            <br></br>
+            <strong>Privacy policy:</strong>
+            <a href="https://business.safety.google/privacy/">https://business.safety.google/privacy/</a>
+            <br></br>
+            <strong>Opt-out link:</strong>
+            <a herf="https://tools.google.com/dlpage/gaoptout">https://tools.google.com/dlpage/gaoptout</a>
+          </p>
+          <p>
+            Category of Personal Information collected according to the CCPA: internet or other electronic
+            network activity information. 
+          </p>
+          <h3>
+            Contacting you
+          </h3>
+          <h4>
+            Contact form (this website)
+          </h4>
+          <p>
+            By filling in the contact form with your data, you authorize this website to use these details to
+            reply to your requests for information, quotes or any other kind of request as indicated by the
+            form’s header.
+          </p>
+          <p>
+            Personal data processed: email address; first name; last name
+          </p>
+          <p>
+            Category of personal information collected according to the CCPA: identifiers. This processing
+            constitutes: a sale according to the CCPA, VCDPA, CPA, CTDPA and UCPA
+          </p>
         </div>
       </>
     );
