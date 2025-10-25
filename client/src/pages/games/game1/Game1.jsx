a89965e7026559209b16516acfcb6b54e64af81c
VM031071 <valerito1973vm@gmail.com>
2025-10-24 20:55:42 -0500

    added initial Game1 page structure and styling

--- client/src/pages/games/game1/Game1.jsx
+++ client/src/pages/games/game1/Game1.jsx
@@ -1,12 +1,32 @@
-function Game1() {
+import "./Game1.css";
+import "./Game1Border.css";
+
+function Game1({ borderState }) {
   console.log("Hello world! (Game1.jsx)");
+
+  // Variables that toggle the class names of specific divs based on the borderState variable.
+  const game1Root = borderState ? "game1RootBorder" : "game1Root";
+  const game1VideoBox = borderState ? "game1VideoBoxBorder" : "game1VideoBox";
   
   return (
     <>
-      <div>
+      <div className={game1Root}>
         <h1>
-          Game 1 Page
+          Roller Ball Battle!
         </h1>
+
+        <div className={game1VideoBox}>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/bD_CCh6ZsFo?si=nHh3Rg45ZdziFiM8"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen>
+          </iframe>
+        </div>
       </div>
     </>
   );
